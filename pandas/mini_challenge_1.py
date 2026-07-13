780acb620fa25ff86222d2fad169b6fe1f45353e
kanhatayal88 <kanhatayal88@gmail.com>
2026-07-13 10:44:25 +0530

    day_2 for road to ML/AI engineer

--- pandas/mini_challenge_1.py
+++ pandas/mini_challenge_1.py
@@ -7,17 +7,14 @@ DATASET_PATH = PROJECT_ROOT / "datasets" / "WA_Fn-UseC_-HR-Employee-Attrition.cs
 
 df = pd.read_csv(DATASET_PATH)
 
-df.columns = [
-    re.sub(r'(?<!^)(?=[A-Z])', '_', col).lower()
-    for col in df.columns
-]
+# normalizing column name from camelCase to snake_case
+df.columns = [re.sub(r'(?<!^)(?=[A-Z])', '_', col).lower() for col in df.columns]
 
 string_columns = df.select_dtypes(include=["object", "string"]).columns
 
-df[string_columns] = df[string_columns].apply(
-    lambda col: col.str.lower().str.strip()
-)
+df[string_columns] = df[string_columns].apply(lambda col: col.str.lower().str.strip())
 
+# check the column name and values that we want to achieve ? 
 print(df.columns)
 print(df.head(20))
 
@@ -61,18 +58,6 @@ print(df.head(20))
 #  'years_since_last_promotion',
 #  'years_with_curr_manager'],
 
-# types of feature 
-# input feature -> business_travel distance_from_home environment_satisfaction job_level job_satisfaction monthly_income num_companies_worked percent_salary_hike performance_rating
-                    # relationship_satisfaction stock_option_level total_working_years work_life_balance years_at_company years_in_current_role years_since_last_promotion years_with_curr_manager
-
-
-
-
-# can remove -> age daily_rate department education education_field employee_count employee_number gender hourly_rate job_involvement marital_status over18 standard_hours
-
-# not sure about feature -> job_role monthly_rate over_time training_times_last_year
-
-# output -> attrition
 
 print(df[["employee_count", "over18", "standard_hours"]])
 
@@ -84,7 +69,7 @@ print(df["standard_hours"].nunique())
 
 # data cleaning
 
-# we dropped this columns because we do not need any columns with constants value.
+# we dropped this columns because we do not need any columns with constant value.
 df.drop(
     columns =[
     "over18",
@@ -93,12 +78,10 @@ df.drop(
     inplace= True
 )
 
-# employee number is a identifier not a feature 
+# employee number is a unique identifier(like primary in Database) not a feature 
 
 print(df["employee_number"].nunique())
 
-# no negative age 
-print(df[df["age"] < 0])
 print(df["age"].min())
 print(df["age"].max())
 
@@ -108,30 +91,29 @@ print(df["age"].max())
 invalid_age = df[df["age"] > 60]
 print(invalid_age)
 
-# check invalid salary
-invalid_salary = df[df["monthly_income"] <= 0]
+# check is there is any one who is doing Unpaid internships
+invalid_salary = df[df["monthly_income"] == 0]
 print(invalid_salary)
 
 # check invalid distance
-invalid_distance = df[df["distance_from_home"] <= 0]
+invalid_distance = df[df["distance_from_home"] < 0]
 print(invalid_distance)
 
 # check invalid experience
 invalid_experience = df[df["years_at_company"] > df["total_working_years"]]
 print(invalid_experience)
 
-# check for invalid roles
+# check for invalid roles means working year company is more than years in curr role in same company, practically not possible
 invalid_role = df[df["years_in_current_role"] > df["years_at_company"]]
 print(invalid_role)
 
-# check for promotion 
 
+# check for promotion 
 invalid_promotion = df[df["years_since_last_promotion"] > df["years_at_company"]]
 print(invalid_promotion)
 
-# check current manager
-
-invalid_manager = df[df["years_with_curr_manager"] < df["years_at_company"]]
+# check current manager, check years with curr manager is greater than years at company 
+invalid_manager = df[df["years_with_curr_manager"] > df["years_at_company"]]
 print(invalid_manager)
 
 invalid_company_count = df[(df["total_working_years"]) <= 0 & (df["num_companies_worked"] > 0)]
@@ -140,20 +122,16 @@ print(invalid_company_count)
 invalid_rating = df[~df["performance_rating"].isin([3, 4])]
 print(invalid_rating)
 
-invalid_rating = df[~df["performance_rating"].isin([3, 4])]
-print(invalid_rating)
-
-# EXPLORATORY DATA ANALYSIS 
-
+# EXPLORATORY DATA ANALYSIS
 print(df["attrition"].value_counts())
 
 print(df["attrition"].value_counts(normalize=True) * 100)
 
-# groupby returns pandas object 
+# groupby returns pandas object
 # It returns a DataFrameGroupBy object.
 # think of it like that we have done grouping now we will get the number
 
-# number of employees who had left the company and the percentage left 
+# number of employees who had left the company and the percentage left
 left_employee = df[df["attrition"] == "yes"]
 print(left_employee)
 department_groups = df.groupby("department")
@@ -166,3 +144,5 @@ left_employees = df[df["attrition"] == "yes"].groupby("department").size()
 attrition_rate = ((left_employees/total_employees)*100).sort_values(ascending=False)
 
 print(attrition_rate)
+print(df.columns)
+print(df[["monthly_income", "over_time"]])
